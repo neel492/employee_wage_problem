140c66d65426d4267b08eb7ff83715c0bc26c076
neelu <neelimatalluri98@gmail.com>
2022-01-10 12:46:58 +0530

    uc2

---
+++ employee_problem.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+isPresent=1
+randomCheck=$((RANDOM%2))
+
+if [ $isPresent -eq $randomCheck ];
+then
+	empRatePerHour=20
+	empHrs=8
+	salary=$(($empRatePerHour*$empHrs))
+else
+	salary=0
+fi
+
+echo $salary
